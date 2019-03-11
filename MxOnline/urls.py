072626b7ad5dede76b1367b824ee4b39b1a1a809
hongbiao <hongb@suchness.cn>
2019-03-11 17:30:52 +0800

    add login

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -13,9 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 import xadmin
+
+from django.views.generic import TemplateView
+from users import views
 urlpatterns = [
     path('xadmin/', xadmin.site.urls),
+    path('', TemplateView.as_view(template_name="index.html"), name="index"),
+    path('login/', views.login, name="login")
+
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib import auth
 
 # Create your views here.
+
+def login(request):
+    if request.method == "POST":
+        user_name = request.POST.get("username")
+        user_pwd = request.POST.get("password")
+        # print("user_name:  ", user_name)
+        # print("user_pwd:  ", user_pwd)
+        # user = auth.authenticate(username=user_name, password=user_pwd)
+        # if user is not None:
+        #     auth.login(request, user)
+        #     return render(request, 'login.html', {})
+        # else:
+        #     return "ERROR"
+        return "ERROR"
+    elif request.method == "GET":
+        user_name = "admin"
+        pass_word  = "123"
+
+        return render(request, 'login.html', {})
